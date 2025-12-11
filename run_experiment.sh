b9fe39ae9abf2fbcc51842a38e62b2f8ee531d5a
Tyler Potyondy <potyondyt@gmail.com>
2025-12-11 12:35:03 -0800

    Add scripts to setup Redox and build OS

--- run_experiment.sh
+++ run_experiment.sh
@@ -2,7 +2,9 @@
 
 # Run setup script, retrying up to 5 times if it fails.
 for i in {1..5}; do
-    ./setup_experiment.sh && break
+    ./setup.sh && break
     echo "Setup script failed, retrying... ($i/5)"
     sleep 2
 done
+
+# make r.drivers && make image && ./run_exp.sh 1
--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 which curl || sudo apt-get install curl
-mkdir -p ~/abacus_redox
-cd ~/abacus_redox
+mkdir -p abacus_redox
+cd abacus_redox
 
 curl -sf https://gitlab.redox-os.org/redox-os/redox/raw/master/native_bootstrap.sh -o native_bootstrap.sh
 
 time bash -e native_bootstrap.sh -y
-
 source ~/.cargo/env
+
+echo "[Abacus Setup Script] - Successfully setup! Now starting build..."
+
+#git remote add abacus https://github.com/abacus-rs/redox.git
+#git checkout abacus/asplos26-eval
+
+# apply patch to drivers recipe to use our fork of the drivers repo.
+#git apply ../cookbook.patch
+
+cd redox
+time make all
