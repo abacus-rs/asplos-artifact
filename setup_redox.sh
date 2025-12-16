75fb94ae0554b351cd32c51afe77e360f9573d3e
Tyler Potyondy <potyondyt@gmail.com>
2025-12-16 08:54:43 -0800

    Fix retry logic in setup_redox script

--- setup_redox.sh
+++ setup_redox.sh
@@ -3,17 +3,16 @@ which curl || sudo apt-get install curl
 mkdir -p abacus_redox
 cd abacus_redox
 
-# Retry on error 5 times.
-for i in {1..5}; do
-  # Run redox provided setup scripts.
-  curl -sf https://gitlab.redox-os.org/redox-os/redox/raw/master/native_bootstrap.sh -o native_bootstrap.sh
-  
-  time bash -e native_bootstrap.sh -y
-  source ~/.cargo/env
-  
-  echo "[Abacus Setup Script] - Successfully setup! Now starting build..."
-  
-  cd redox
-  time make all
+set -e
 
-done
+# Run redox provided setup scripts.
+curl -sf https://gitlab.redox-os.org/redox-os/redox/raw/master/native_bootstrap.sh -o native_bootstrap.sh
+
+time bash -e native_bootstrap.sh -y
+source ~/.cargo/env
+
+echo "[Abacus Setup Script] - Successfully setup! Now starting build..."
+
+cd redox
+time make all
+
